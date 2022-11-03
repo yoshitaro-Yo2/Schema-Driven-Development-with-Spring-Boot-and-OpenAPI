c87d7254f4d1d4771233e78d726cf2e28b4a0963
yoshitaro-Yo2 <kichivs.mp@gmail.com>
2022-11-03 21:09:40 +0900

    自動生成されたインターフェイスを実装する

--- src/main/java/com/example/todoapi/controller/HealthController.java
+++ src/main/java/com/example/todoapi/controller/HealthController.java
@@ -1,4 +1,15 @@
 package com.example.todoapi.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController implements HealthApi {
+
+    // インターフェイスでは デフォルト実装されているメソッド。まだ実装されていないというモック
+    // return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    @Override
+    public ResponseEntity<Void> healthGet() {
+        return ResponseEntity.ok().build();
+    }
 }
