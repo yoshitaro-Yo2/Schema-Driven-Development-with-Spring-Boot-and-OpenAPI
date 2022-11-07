6559f8538b1cb1aca47a3d5cd18998844b0eb17c
yoshitaro-Yo2 <kichivs.mp@gmail.com>
2022-11-07 17:30:40 +0900

    RESTController 作成 Postman確認用

---
+++ src/main/java/com/example/todoapi/controller/sample/SampleController.java
@@ -0,0 +1,19 @@
+package com.example.todoapi.controller.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// 合成アノテーション @ResponseBody
+// ハンドラーメソッドの戻り値がレスポンスボディに書き出されるアノテーション
+// Postman でレスポンスボディ見れるよになる
+@RestController
+@RequestMapping("/samples")
+public class SampleController {
+
+    @GetMapping
+    public String index() {
+        return "ok!";
+    }
+
+}
