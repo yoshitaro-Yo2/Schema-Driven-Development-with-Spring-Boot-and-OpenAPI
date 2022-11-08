ef720d6e8ee44c0cf678d068cc9173dc63b9acd7
yoshitaro-Yo2 <kichivs.mp@gmail.com>
2022-11-08 11:03:56 +0900

    スキーマから自動生成された インターフェイスの実装

---
+++ src/main/java/com/example/todoapi/controller/tasks/TaskController.java
@@ -0,0 +1,16 @@
+package com.example.todoapi.controller.tasks;
+
+import com.example.todoapi.controller.TasksApi;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController implements TasksApi {
+
+    @Override
+    public ResponseEntity<Void> tasks1Get() {
+        return ResponseEntity.ok().build();
+    }
+}
+
